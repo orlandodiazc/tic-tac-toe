91e5c3d6082095e21e81ad03afd156419e4c2f61
Orlando Diaz <orlandodiazconde@gmail.com>
2023-05-27 14:42:06 -0500

    Update router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import Board from "./pages/Board";
 import Home from "./pages/Home";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
@@ -11,8 +16,20 @@ const router = createBrowserRouter([
     element: <Home />,
   },
   {
-    path: "/play",
+    path: "/play/pvp",
     element: <Board />,
+  },
+  {
+    path: "/play/cpu",
+    element: <Board />,
+  },
+  {
+    path: "/play/",
+    element: <Navigate to="/play/pvp" />,
+  },
+  {
+    path: "*",
+    element: <ErrorPage />,
   },
 ]);
 
