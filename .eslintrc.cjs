24ed3e5979819c45756dd46d81064aa098b3d19e
Orlando Diaz <orlandodiazconde@gmail.com>
2023-05-29 11:09:49 -0500

    Update config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,20 @@ module.exports = {
     browser: true,
     es2021: true,
   },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   extends: ["plugin:react/recommended", "standard-with-typescript", "prettier"],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
+    project: ["tsconfig.json"],
   },
   plugins: ["react"],
-  rules: {},
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
 };
